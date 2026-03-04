bf45fabcdbc1ec69ac914fa606cd8b73e7b5754c
harshith-venkatesh <harshithece7016@gmail.com>
2026-03-04 17:08:40 +0530

    feat: update dashboard sidebar changes

--- features/dashboard/components/dashboard-sidebar.tsx
+++ features/dashboard/components/dashboard-sidebar.tsx
@@ -1,13 +1,36 @@
 "use client";
 
-import {usePathname} from "next/navigation";
-import { Sidebar,SidebarContent,SidebarFooter,SidebarInset, useSidebar, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail, SidebarTrigger} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarInset,
+  useSidebar,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import Image from "next/image";
-import {Link, LucideIcon, User} from "lucide-react";
-import {Button} from "@/components/ui/button";
-import {Skeleton} from "@/components/ui/skeleton";
-import {OrganizationSwitcher, useClerk, UserButton} from "@clerk/nextjs";
-import { Home, LayoutGrid, AudioLines, Volume2, Settings, Headphones } from "lucide-react";
+import Link from "next/link";
+import { LucideIcon, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { OrganizationSwitcher, useClerk, UserButton } from "@clerk/nextjs";
+import {
+  Home,
+  LayoutGrid,
+  AudioLines,
+  Volume2,
+  Settings,
+  Headphones,
+} from "lucide-react";
 
 interface MenuItem {
   title: string;
@@ -17,7 +40,7 @@ interface MenuItem {
 }
 
 interface NavSectionProps {
-  label ?: string;
+  label?: string;
   items: MenuItem[];
   pathname: string;
 }
@@ -25,30 +48,48 @@ interface NavSectionProps {
 function NavSection({ label, items, pathname }: NavSectionProps) {
   return (
     <SidebarGroup>
-      {label && <SidebarGroupLabel className="text-[13px] uppercase text-muted-foreground">{label}</SidebarGroupLabel>}
+      {label && (
+        <SidebarGroupLabel className="text-[13px] uppercase text-muted-foreground">
+          {label}
+        </SidebarGroupLabel>
+      )}
       <SidebarGroupContent>
         <SidebarMenu>
           {items.map((item) => (
-            <SidebarMenuItem key={item.title} >
-              <SidebarMenuButton asChild={!!item.url} onClick={item.onClick} isActive={item.url ? item.url === '/' ? pathname === '/' : pathname.startsWith(item.url) : false} tooltip={item.title}>
-              {item.url ? (
-                <Link href={item.url}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </Link>
-              ) : (
-                <div className="flex items-center gap-2">
-                  <item.icon  />
-                 <span> {item.title}</span>
-                </div>
-              )}
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild={!!item.url}
+                onClick={item.onClick}
+                isActive={
+                  item.url
+                    ? item.url === "/"
+                      ? pathname === "/"
+                      : pathname.startsWith(item.url)
+                    : false
+                }
+                tooltip={item.title}
+                 className="h-9 px-3 py-2 text-[13px] tracking-tight font-medium border border-transparent data-[active=true]:border-border data-[active=true]:shadow-[0px_1px_1px_0px_rgba(44,54,53,0.03),inset_0px_0px_0px_2px_white]"
+              >
+                {item.url ? (
+                  
+                    <Link href={item.url} >
+                      <item.icon width={16} height={16}/>
+                      <span>{item.title}</span>
+                    </Link>
+                  
+                ) : (
+                  <>
+                    <item.icon width={16} height={16} />
+                    <span> {item.title}</span> 
+                  </>
+                )}
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
-  )
+  );
 }
 
 export function DashboardSidebar() {
@@ -61,35 +102,74 @@ export function DashboardSidebar() {
     { title: "Explore Voices", icon: LayoutGrid, url: "/voices" },
     { title: "Text to Speech", icon: AudioLines, url: "/text-to-speech" },
     { title: "Voice Cloning", icon: Volume2 },
-    
   ];
 
   const othersMenuItems: MenuItem[] = [
-    { title: "Settings", icon: Settings, onClick: () => clerk.openOrganizationProfile() },
-    { title: "Help and Support", icon: Headphones, url: "mailto:harshith.venkatesh6@gmail.com" },
+    {
+      title: "Settings",
+      icon: Settings,
+      onClick: () => clerk.openOrganizationProfile(),
+    },
+    {
+      title: "Help and Support",
+      icon: Headphones,
+      url: "mailto:harshith.venkatesh6@gmail.com",
+    },
   ];
 
   return (
     <Sidebar collapsible="icon">
-      <SidebarInset className="min-h-0 min-w-0">
-        <SidebarHeader className="flex flex-col gap-4 pt-2">
-          <div className="flex items-center gap-2 pl-1 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:pl-0">
-
-            <Image src="/logo.svg" alt="Logo" width={24} height={24} className="rounded-sm" />
-            <span className="font-semibold text-lg tracking-lighter text-foreground group-data-[collapsible=icon]:hidden">
-              Resonance
-            </span>
-            <SidebarTrigger className="ml-auto lg:hidden" />
-          </div>
-
-        </SidebarHeader>
-        <SidebarContent>
-          <NavSection items={mainMenuItems} pathname={pathname} />
-          <NavSection label="Others" items={othersMenuItems} pathname={pathname} />
-        </SidebarContent>
-          
-        </SidebarInset>
-       
+      <SidebarHeader className="flex flex-col gap-4 pt-2">
+        <div className="flex items-center gap-2 pl-1 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:pl-0">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={24}
+            height={24}
+            className="rounded-sm"
+          />
+          <span className="group-data-[collapsible=icon]:hidden font-semibold text-lg tracking-lighter text-foreground">
+            Resonance 
+            
+          </span>
+          <SidebarTrigger className="ml-auto lg:hidden" />
+        </div>
+       <SidebarMenu>
+          <SidebarMenuItem>
+            <OrganizationSwitcher
+              hidePersonal
+              fallback={
+                <Skeleton
+                  className="h-8.5 w-full group-data-[collapsible=icon]:size-8 rounded-md border bg-white"
+                />
+              }
+              appearance={{
+                elements: {
+                  rootBox: 
+                    "w-full! group-data-[collapsible=icon]:w-auto! group-data-[collapsible=icon]:flex! group-data-[collapsible=icon]:justify-center!",
+                  organizationSwitcherTrigger:
+                    "w-full! justify-between! bg-white! border! border-border! rounded-md! pl-1! pr-2! py-1! gap-3! group-data-[collapsible=icon]:w-auto! group-data-[collapsible=icon]:p-1! shadow-[0px_1px_1.5px_0px_rgba(44,54,53,0.03)]!",
+                  organizationPreview: "gap-2!",
+                  organizationPreviewAvatarBox: "size-6! rounded-sm!",
+                  organizationPreviewTextContainer: 
+                    "text-xs! tracking-tight! font-medium! text-foreground! group-data-[collapsible=icon]:hidden!",
+                  organizationPreviewMainIdentifier: "text-[13px]!",
+                  organizationSwitcherTriggerIcon:
+                    "size-4! text-sidebar-foreground! group-data-[collapsible=icon]:hidden!",
+                },
+              }}
+            />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavSection items={mainMenuItems} pathname={pathname} />
+        <NavSection
+          label="Others"
+          items={othersMenuItems}
+          pathname={pathname}
+        />
+      </SidebarContent>
     </Sidebar>
   );
 }
